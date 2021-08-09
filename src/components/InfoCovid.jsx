33861245708a312dae138c140005f9e4a5c3245a
Jonathan <jonathanestebangalan@gmail.com>
2021-08-09 16:36:16 +0200

    añadido un custom hook al componente InfoCovid

--- src/components/InfoCovid.jsx
+++ src/components/InfoCovid.jsx
@@ -1,31 +1,13 @@
-import React, { useState, useEffect, useContext, memo } from "react";
+import React, { useContext } from "react";
 import { DateOrganizedContext } from "../context/DateOrganized";
-import { TodayContext } from "../context/DateToday";
+import {useInfoCovid} from '../hooks/useInfoCovid'
 import { Paragraph } from "../styles/Paragrahph";
 import SectionStyled from "../styles/Section";
 
-const InfoCovid = memo(() => {
-  const { day } = useContext(TodayContext);
+const InfoCovid = () => {
   const { dateCorrect } = useContext(DateOrganizedContext);
 
-  const [infoSpain, setInfoSpain] = useState([]);
-
-  useEffect(() => {
-    const fetching = async () => {
-      if (day) {
-        const res = await fetch(
-          `https://api.covid19tracking.narrativa.com/api/${day}/country/spain`
-        );
-        const data = await res.json();
-        const { dates } = await data;
-        const datesArray = Object.entries(dates);
-        const datesObject = datesArray[0][1];
-        const { countries } = datesObject;
-        setInfoSpain(countries.Spain);
-      }
-    };
-    fetching();
-  }, [day]);
+  const [infoSpain] = useInfoCovid()
 
   return (
     <>
@@ -39,6 +21,6 @@ const InfoCovid = memo(() => {
       </SectionStyled>
     </>
   );
-});
+};
 
 export default InfoCovid;
---
+++ src/hooks/useInfoCovid.js
@@ -0,0 +1,25 @@
+import { useState, useEffect, useContext } from 'react'
+import { TodayContext } from '../context/DateToday';
+
+export const useInfoCovid = () => {
+  const { day } = useContext(TodayContext)
+  const [infoSpain, setInfoSpain] = useState([])
+
+  useEffect(() => {
+    const fetching = async () => {
+      if (day) {
+        const res = await fetch(
+          `https://api.covid19tracking.narrativa.com/api/${day}/country/spain`
+        );
+        const data = await res.json();
+        const { dates } = await data;
+        const datesArray = Object.entries(dates);
+        const datesObject = datesArray[0][1];
+        const { countries } = datesObject;
+        setInfoSpain(countries.Spain);
+      }
+    };
+    fetching();
+  }, [day]);
+  return[infoSpain]
+}
